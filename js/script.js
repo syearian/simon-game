4e4724fc2c2e91d08ecbbadfb93cdecdf4a4a610
Steven Yearian <syearian@gmail.com>
2016-07-27 15:13:34 -0500

    Add logic to playAudio and flashColor

--- js/script.js
+++ js/script.js
@@ -1,18 +1,75 @@
 var strict = false;
-var greenAudio;
-var redAudio;
-var yellowAudio;
-var blueAudio;
+var green = document.getElementById('green');
+var red = document.getElementById('red');
+var yellow = document.getElementById('yellow');
+var blue = document.getElementById('blue');
 var count = '--';
 var series = [];
 var playerAttempt = series;
 
-function playAudio() {
-
+function playAudio(button) {
+  var audio = button.firstElementChild;
+  audio.play();
 }
 
-function flashColor() {
+// From https://css-tricks.com/snippets/javascript/lighten-darken-color/
+function LightenDarkenColor(col, amt) {
+  
+    var usePound = false;
+  
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + String("000000" + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
 
+  
+}
+
+function getHexColor(color) {
+  var hexColor;
+  switch (color) {
+    case 'green':
+      hexColor = '#008000';
+      break;
+    case 'red':
+      hexColor = '#FF0000';
+      break;
+    case 'yellow':
+      hexColor = '#FFFF00';
+      break;
+    case 'blue':
+      hexColor = '#0000FF';
+      break;
+  }
+  return hexColor;
+}
+
+function flashColor(button) {
+  var color = getHexColor(button.id);
+  var flashColor = LightenDarkenColor(color, 10);
+  button.style.background = flashColor;
+  setTimeout(function(button, color) {
+    button.style.background = color;
+  }, 500);
 }
 
 function incrementCount() {
@@ -36,8 +93,9 @@ function addToSeries() {
 
 }
 
-function pressButton() {
-
+function pressButton(button) {
+  playAudio(button);
+  flashColor(button);
 }
 
 function startSeries() {
